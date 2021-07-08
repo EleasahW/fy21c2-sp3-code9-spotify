a5e25ff0194cd672f9d594aa266626ecf6d23362
EleasahW <eleasahwhittaker@gmail.com>
2021-07-08 09:22:11 -0400

    js - console recognition

---
+++ my.js
@@ -0,0 +1,15 @@
+// Pseudo code
+
+// 1 Make home-popup pop up in center
+
+const homeOverlay = document.querySelector('#home-popup');
+console.log(homeOverlay);
+
+
+// 2 Make login pop up in center on:click (login)
+/*const loginOverlay = document.querySelector('#login-popup');
+console.log(loginOverlay);*/
+
+// 3 Check if input boxes are empty
+
+// 4 Check if length is correct
