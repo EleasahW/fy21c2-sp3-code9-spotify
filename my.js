68ccf57d1f72195e8967c355611d354a7a240570
EleasahW <eleasahwhittaker@gmail.com>
2021-07-08 10:33:13 -0400

    js - begun

--- my.js
+++ my.js
@@ -2,14 +2,34 @@
 
 // 1 Make home-popup pop up in center
 
+
+// 1. connect home-popup w/ console
 const homeOverlay = document.querySelector('#home-popup');
 console.log(homeOverlay);
 
 
-// 2 Make login pop up in center on:click (login)
+// 2. have an overlay that pops up on load
+
+function on() {
+    document.getElementById(homeOverlay).addEventListener("load", on())
+    document.getElementById(homeOverlay).style.display = "block";
+
+}
+
+// 3. connect login-popup w/ console
 /*const loginOverlay = document.querySelector('#login-popup');
 console.log(loginOverlay);*/
 
-// 3 Check if input boxes are empty
 
-// 4 Check if length is correct
+// 4. have an overlay that pops up on click on button
+
+
+// 5. Check if input box length is correct
+
+// 6. have both overlays off on click of log-in button
+
+/*function off() {
+    document.getElementById(homeOverlay).style.display = "none";
+}*/
+
+
