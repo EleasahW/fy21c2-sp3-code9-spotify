2f0c88ead6c476059732b1d4246264c27037451b
EleasahW <eleasahwhittaker@gmail.com>
2021-07-08 18:31:27 -0400

    1. initial popup (homeOverlay): complete. 2. secondary popup (loginOverlay): complete.

--- my.js
+++ my.js
@@ -1,23 +1,25 @@
 // Pseudo code
 
-// 1 Make home-popup pop up in center
+// Goal: Make Functional Home Overlay
 // 1. connect home-popup w/ console
 const homeOverlay = document.querySelector('#home-popup');
 console.log(homeOverlay);
 
-
-// 2. have an overlay that pops up on load
-document.getElementById(homeOverlay).addEventListener("load", homeOverlayOn);
-
-function homeOverlayOn() {
-    document.getElementById(homeOverlay).style.display = "block";
-
-}
+// 2. have an overlay that pops up on load: when the page loads, the hidden on the homeOverlay (#home-popup) will go away.
+window.addEventListener("load", function (){
+    homeOverlay.classList.remove('hidden');
+    });
 
 // 3. connect login-popup w/ console
-/*const loginOverlay = document.querySelector('#login-popup');
-console.log(loginOverlay);*/
+const loginOverlay = document.querySelector('#login-popup');
+console.log(loginOverlay);
 
+const loginBtn = document.querySelector('#login-popup-portal')
+
+loginBtn.addEventListener("click", function (){
+    homeOverlay.classList.add('hidden');
+    loginOverlay.classList.remove('hidden');
+});
 
 // 4. have an overlay that pops up on click on button
 
